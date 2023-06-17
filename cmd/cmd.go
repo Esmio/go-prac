cf909d6ee038ba7ecbe71db3af04150f50b47fbd
esmio <none@no.no>
2023-06-17 19:31:48 +0900

    add spf13/cobra

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,15 +1,49 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"mongosteen/internal/database"
 	"mongosteen/internal/router"
+
+	"github.com/spf13/cobra"
 )
 
+func Run() {
+	rootCmd := &cobra.Command{
+		Use: "mongosteen",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("hi")
+		},
+	}
+
+	srvCmd := &cobra.Command{
+		Use: "server",
+		Run: func(cmd *cobra.Command, args []string) {
+			RunServer()
+		},
+	}
+
+	dbCmd := &cobra.Command{
+		Use: "db",
+	}
+
+	createCmd := &cobra.Command{
+		Use: "create",
+		Run: func(cmd *cobra.Command, args []string) {
+			database.PgCreateTables()
+		},
+	}
+
+	rootCmd.AddCommand(srvCmd)
+	rootCmd.AddCommand(dbCmd)
+	dbCmd.AddCommand(createCmd)
+	database.PgConnect()
+	defer database.PgClose()
+	rootCmd.Execute()
+}
+
 func RunServer() {
-	database.MysqlConnect()
-	database.MysqlCreateTable()
-	defer database.MysqlClose()
 	r := router.New()
 	err := r.Run(":8080")
 	if err != nil {
