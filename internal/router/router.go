2c30ae314e6fc55d9c2d692f8cc4bfa72e7e1b2f
esmio <none@no.no>
2023-06-26 02:02:05 +0900

    add sh

--- internal/router/router.go
+++ internal/router/router.go
@@ -10,25 +10,6 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-// @title           Mongosteen API
-// @version         1.0
-// @description     This is a sample server celler server.
-// @termsOfService  http://swagger.io/terms/
-
-// @contact.name   Simon
-// @contact.url    https://dongxiaoming.com
-// @contact.email  support@swagger.io
-
-// @license.name  MIT
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host      localhost:8080
-// @BasePath  /api/v1
-
-// @securityDefinitions.basic  BasicAuth(JWT)
-
-// @externalDocs.description  OpenAPI
-// @externalDocs.url          https://swagger.io/resources/open-api/
 func New() *gin.Engine {
 	docs.SwaggerInfo.Version = "1.0"
 	r := gin.Default()
--- main.go
+++ main.go
@@ -4,6 +4,26 @@ import (
 	"mongosteen/cmd"
 )
 
+// @title           Mongosteen API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Simon
+// @contact.url    https://dongxiaoming.com
+// @contact.email  support@swagger.io
+
+// @license.name  MIT
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth(JWT)
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
+
 func main() {
 	cmd.Run()
 }
