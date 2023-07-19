0af39c5fefdf5d7141f05b205d87c14981018071
esmio <none@no.no>
2023-07-20 02:51:01 +0900

    修复测试小问题

--- internal/controller/ping.go
+++ internal/controller/ping.go
@@ -6,16 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Ping godoc
-// @Summary      测试 API 是否可以正常工作
-// @Description  测试 API 是否可以正常工作
-// @Tags         Ping
-// @Accept       json
-// @Produce      json
-// @Param        id   path      int  false  "Account ID"
-// @Success      200
-// @Failure      500
-// @Router       /ping [get]
 func Ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 
