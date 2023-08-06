58ad254ddf05f764a294d2265ff1f0cd547243e9
esmio <none@no.no>
2023-08-07 00:37:24 +0900

    创建 Item

--- config/schema.sql
+++ config/schema.sql
@@ -15,3 +15,15 @@ CREATE TABLE validation_codes (
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TYPE kind as ENUM ('expenses', 'in_come');
+CREATE TABLE items (
+    id BIGSERIAL PRIMARY KEY,
+    user_id SERIAL NOT NULL,
+    amount INTEGER NOT NULL,
+    tag_ids INTEGER[] NOT NULL,
+    kind kind NOT NULL,
+    happened_at TIMESTAMP NOT NULL DEFAULT now(),
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
