a1027c7bd4d669343062b590d81e22870b8a671b
esmio <none@no.no>
2023-10-01 23:00:35 +0900

    upate tag if param is empty, use former param value.

--- internal/controller/tag_controller_test.go
+++ internal/controller/tag_controller_test.go
@@ -87,5 +87,6 @@ func TestUpdateTag(t *testing.T) {
 	assert.Equal(t, u.ID, j.Resource.UserID)
 	assert.Equal(t, "吃饭", j.Resource.Name)
 	assert.Equal(t, "🚌", j.Resource.Sign)
+	assert.Equal(t, "expenses", j.Resource.Kind)
 	assert.Nil(t, j.Resource.DeletedAt)
 }
